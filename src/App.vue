1b0546d2dd2379b27bac893a4c8ccc33fba54134
tocausan <tocausan@gmail.com>
2023-01-31 21:55:44 +0100

    routes & responsive

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <NavComponent/>
-  <router-view/>
-  <FooterComponent/>
+  <NavComponent />
+  <router-view />
+  <FooterComponent />
 </template>
 
 <style lang="scss">
+* {
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -13,20 +17,26 @@
   color: #2c3e50;
   width: 100vw;
   height: 100vh;
+  overflow: scroll;
+}
+
+@media screen and (max-width: 768px) {
+  #app {
+    height: inherit;
+  }
 }
 </style>
 
-
 <script lang="ts">
-import { defineComponent } from 'vue';
-import NavComponent from '@/components/NavComponent.vue';
-import FooterComponent from './components/FooterComponent.vue';
+import { defineComponent } from "vue";
+import NavComponent from "@/components/NavComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 
 export default defineComponent({
-  name: 'App',
+  name: "App",
   components: {
     NavComponent,
-    FooterComponent
+    FooterComponent,
   },
 });
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,33 +1,42 @@
 <template>
   <div class="home">
     <a href="mailto:hello@acte.co">hello@acte.co</a>
-   </div>
+  </div>
 </template>
 
-<style lang="scss">
-.home{
+<style lang="scss" scoped>
+.home {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100%;
   width: 100%;
+  height: 100%;
+  overflow: hidden;
 
-  a{
+  a {
     font-weight: 800;
     font-size: 1.2rem;
-    letter-spacing: .05rem;
-    font-family: 'Roboto', sans-serif;
+    letter-spacing: 0.05rem;
+    font-family: "Roboto", sans-serif;
+  }
+}
+
+@media screen and (max-width: 768px) {
+  .home {
+    padding-top: 200px;
+
+    a {
+      font-weight: inherit;
+    }
   }
 }
 </style>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'HomeView',
-  components: {
-    
-  },
+  name: "HomeView",
+  components: {},
 });
 </script>
